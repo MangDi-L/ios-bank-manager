9eeedc938c70b48ad516f837afdb16e4cf84a03d
Jae Hyung Kim <smfc.mene@gmail.com>
2022-11-09 22:40:12 +0800

    feat: buttonStackView에 들어갈 add, clear 버튼 구현

--- BankManagerUIApp/BankManagerUIApp/Controller/ViewController.swift
+++ BankManagerUIApp/BankManagerUIApp/Controller/ViewController.swift
@@ -7,7 +7,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     let mainStackView: UIStackView = {
         let stackView = UIStackView()
         stackView.translatesAutoresizingMaskIntoConstraints = false
@@ -15,12 +15,26 @@ class ViewController: UIViewController {
         stackView.spacing = 8
         return stackView
     }()
-    
     let buttonStackView: UIStackView = {
         let stackView = UIStackView()
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
         return stackView
+    }()
+    let addButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("고객 10명 추가", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    let clearButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("초기화", for: .normal)
+        button.setTitleColor(.red, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
     }()
     
     let timerStackView: UIStackView = {
@@ -46,6 +60,25 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        addSubview()
+        setupUIConstraints()
+    }
+    
+    private func addSubview() {
+        self.view.addSubview(mainStackView)
+        mainStackView.addArrangedSubview(buttonStackView)
+        mainStackView.addArrangedSubview(timerStackView)
+        mainStackView.addArrangedSubview(queueStateStackView)
+        mainStackView.addArrangedSubview(queueStackView)
         
+        buttonStackView.addArrangedSubview(addButton)
+        buttonStackView.addArrangedSubview(clearButton)
+    }
+    
+    private func setupUIConstraints() {
+        mainStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        mainStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        mainStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        mainStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
     }
 }
